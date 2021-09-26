9a2bf865002a67080d60791839c1f5d5ddcaa436
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-26 22:14:18 +0530

    Create Library.java

---
+++ classObjectMethod/Library.java
@@ -0,0 +1,34 @@
+public class Library //class
+{
+
+public static void main(String[] args)
+{
+
+Library librarian = new Library();// object
+
+librarian.lend();
+librarian.addMember();
+
+}
+
+public void lend() //method
+{
+System.out.println("to Take BooK");
+}
+public void addMember()
+{
+System.out.println("to add Member");
+}
+
+
+}
+
+/* 
+
+output :-
+
+     to Take BooK
+     to add Member
+
+
+*/
