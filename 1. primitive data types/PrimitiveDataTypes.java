5d702c2ccbd33f6e65790935fbf9abfec4985f8e
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-27 07:51:51 +0530

    Create PrimitiveDataTypes.java

---
+++ 1. primitive data types/PrimitiveDataTypes.java
@@ -0,0 +1,46 @@
+public class PrimitiveDataTypes
+{
+
+public static void main(String[] args)
+{
+
+	char initial = 'P';
+	int age = 21;
+	  byte age1 = 22;
+	  short age2 = 23;
+	  long age3 = 24;
+	float height = 6.2f;
+	  double height1 = 6.2;
+	boolean joinFamily = false;
+	
+	
+	System.out.println("initial " + initial);
+	System.out.println("age " + age);
+	System.out.println("age " + age1);
+	System.out.println("age " + age2);
+	System.out.println("age " + age3);
+	System.out.println("height " + height);
+	System.out.println("height " + height1);
+	System.out.println("joinFamily " + joinFamily);
+
+}
+
+
+}
+
+
+/*
+
+output:-
+
+initial P
+age 21
+age 22
+age 23
+age 24
+height 6.2
+height 6.2
+joinFamily false
+
+
+*/
