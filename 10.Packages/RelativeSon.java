6b65b5f1f3f396915411508ef31766ef1c1e3524
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-27 14:07:21 +0530

    Create RelativeSon.java

---
+++ 10.Packages/RelativeSon.java
@@ -0,0 +1,26 @@
+package tamilnadu.nativeplace;
+
+//same package donot import
+
+public class RelativeSon extends Relatives
+{
+public static void main(String[] args)
+{
+
+RelativeSon rs = new RelativeSon();
+rs.doGardening();
+rs.receivePeople();
+}
+
+
+}
+
+/*
+
+output:-
+
+Gardening
+Receive
+
+
+*/
