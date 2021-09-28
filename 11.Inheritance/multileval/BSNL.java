b46c2814891ebf5dfc249d8aa28f91f09e9fe3f4
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-28 19:33:02 +0530

    Create BSNL.java

---
+++ 11.Inheritance/multileval/BSNL.java
@@ -0,0 +1,23 @@
+public class BSNL
+{
+public void recruit()
+{
+}
+
+}
+
+
+public class MoblileNW extends BSNL
+{
+
+}
+
+public class LandlineNW extends BSNL
+{
+
+}
+
+public class BroadBand extends BSNL
+{
+
+}
