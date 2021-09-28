aa859a47b0eb1af9b55b7354927d017f47a197bb
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-28 19:35:20 +0530

    Create Myself.java

---
+++ 11.Inheritance/multileval/Myself.java
@@ -0,0 +1,24 @@
+public class Myself extends Parent
+{
+
+public static void main(String[] args)
+{
+Myself ragul = new Myself();
+ragul.smile();//call GGP class
+ragul.beBold();//call GP clas
+ragul.work();//call Parent class
+
+}
+}
+
+
+/*
+
+output:-
+
+smile
+be bold
+working
+
+
+*/
