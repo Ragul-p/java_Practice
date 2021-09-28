10f5e6d84d1dc2f2def59d574871e5e4b7b6c312
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-28 19:28:10 +0530

    Create RelativeDaughter.java

---
+++ 10.packages/RelativeDaughter.java
@@ -0,0 +1,26 @@
+package tamilnadu.chennai;
+
+import tamilnadu.nativeplace.Relatives;//different package to import
+
+public class RelativeDaughter extends Relatives
+{
+public static void main(String[] args)
+{
+
+RelativeDaughter rd = new RelativeDaughter();
+rd.doGardening();
+rd.receivePeople();
+}
+
+}
+
+
+/*
+
+output:-
+
+Gardening
+Receive
+
+
+*/
