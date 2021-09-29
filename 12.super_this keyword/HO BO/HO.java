b7f1c8d7ec297b29b58cf42ec62e2578565f96d6
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-29 21:38:53 +0530

    Create HO.java

---
+++ 12.super_this keyword/HO BO/HO.java
@@ -0,0 +1,15 @@
+public class HO
+{
+
+int offer = 1000;
+public HO()
+{
+	
+}
+public HO(int price ,int discount)
+{
+System.out.println("HO " + price);
+System.out.println("HO " + discount);
+}
+
+}
