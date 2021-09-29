b623859109a45b93b17cb1df04f51021e43ecc48
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-29 21:37:57 +0530

    Create Child.java

---
+++ 12.super_this keyword/Child.java
@@ -0,0 +1,36 @@
+public class Child
+{
+
+public Child()
+{
+//this(100);
+System.out.println("welcome to our shop");
+}
+	
+public Child(int price)
+{
+this();// current class cunstructor explicit call
+System.out.println("price " + price);	
+}
+
+public static void main(String[] args)
+{
+
+//Child ch =new Child();
+Child ch1 =new Child(100);
+Child ch2 =new Child();
+}
+
+
+}
+
+/*
+
+output:-
+
+welcome to our shop
+price 100
+welcome to our shop
+
+
+*/
