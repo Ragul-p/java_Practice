017ea9966621c69a2031d57a8e8f1ca784c808c6
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-29 21:42:27 +0530

    Create Parent.java

---
+++ 12.super_this keyword/Parent Child/Parent.java
@@ -0,0 +1,21 @@
+public class Parent
+{
+
+	
+//default constructor
+public Parent()
+{
+
+System.out.println("parent default constructor");
+}
+
+	
+//parametrize constructor
+public Parent(String name)
+{
+System.out.println("parent parametrize constructor"); 
+}
+
+
+
+}
