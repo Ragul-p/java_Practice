643f6b1ee445930033b7980ea2a248d9418b3cb1
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-30 14:38:04 +0530

    Create Parent.java

---
+++ 14.Dynamic and Static Binding/Parent Child/Parent.java
@@ -0,0 +1,26 @@
+public class Parent
+{
+
+int i = 100;
+static int j = 200;
+	
+String mobile = "normal phone";
+	
+public void watchNews()
+{
+System.out.println("parent class tv news");
+}
+
+public void takeRest()
+{
+System.out.println("parent class rest");
+}
+
+public static void work()
+{
+System.out.println("child class working child");
+}
+
+
+
+}
