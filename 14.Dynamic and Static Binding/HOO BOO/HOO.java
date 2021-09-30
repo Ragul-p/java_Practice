4094c8ed98a1adc5c442e67ff49b7e851ba08adc
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-09-30 14:36:48 +0530

    Create HOO.java

---
+++ 14.Dynamic and Static Binding/HOO BOO/HOO.java
@@ -0,0 +1,20 @@
+public class HOO
+{
+	int bonus  = 1000;
+
+public void develop()
+{
+System.out.println("develop App ");
+}
+
+public void test()
+{
+System.out.println("test App ");
+}
+	
+public void pongal()
+{
+System.out.println(" HOO pongal bonus " + bonus);
+}
+
+}
