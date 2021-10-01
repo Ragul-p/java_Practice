a4505498499294442c3bbb7c694eecc5b2330930
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-01 21:33:42 +0530

    Create A.java

---
+++ 19.Abstraction/A B/A.java
@@ -0,0 +1,20 @@
+public abstract class A
+{
+
+	public abstract void m1();
+/*
+
+public abstract void m1()
+{
+System.out.println("m1  A ");
+}
+error: abstract methods cannot have a body
+
+*/
+
+public void m2()
+{
+System.out.println("m2  A ");
+}
+
+}
