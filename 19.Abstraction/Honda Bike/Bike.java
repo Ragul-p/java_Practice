f2221bfa30979dbd128ec071a010ede10e1c7bca
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-01 21:38:50 +0530

    Create Bike.java

---
+++ 19.Abstraction/Honda Bike/Bike.java
@@ -0,0 +1,13 @@
+public abstract class Bike
+
+{
+int oilPrice=10;
+// int abstract a=10;  error: <identifier> expected
+
+	
+public abstract void ride();
+
+public abstract void acceleration();
+public abstract void milage();
+
+}
