378e4b21a51cd33378a503e4bd98109ffc224f31
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-01 21:37:53 +0530

    Create MainClass.java

---
+++ 19.Abstraction/Demo MainClass/MainClass.java
@@ -0,0 +1,26 @@
+public class MainClass extends Demo
+{
+
+public static void main(String[] args)
+{
+
+Demo d = new MainClass();
+d.display();
+
+}
+
+public void display()
+{
+System.out.println("main class");
+}
+
+}
+
+/*
+
+output:-
+
+main class
+
+
+*/
