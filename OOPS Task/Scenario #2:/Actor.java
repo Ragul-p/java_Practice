a0c9cca5b13ab406d579492ea4828424b3c64385
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-03 15:10:58 +0530

    Create Actor.java

---
+++ OOPS Task/Scenario #2:/Actor.java
@@ -0,0 +1,17 @@
+interface Actor   //-------------------1.Create an interface called ‘Actor’
+{
+
+	
+
+	
+boolean makeupRequired = true; //------2.Have variables boolean makeUpRequired ---3.Assign ‘true’ value for ‘makeUpRequired’
+
+String address = " Chennai"; //--------4.Have variable String address. -----------5.Assign value as “Chennai” to address.
+	
+	
+//-------------------------------------6.Have void methods act(), dance(), sing()
+public void act();
+public void dance();
+public void sing();
+
+}
