2dfb56e08c7cc14a3deb4927fc055a0afa2cd5d7
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-03 15:26:44 +0530

    Create trafficRulesDelhi.java

---
+++ OOPS Task/Scenario #5:/trafficRulesDelhi.java
@@ -0,0 +1,19 @@
+package india.newDelhi;  //---------------------9.Now, create another package called ‘india.newDelhi’
+
+
+interface trafficRulesDelhi //-----------------10.Create an interface ‘TrafficRulesDelhi’ under this package
+{                           //-----------------11.Make sure this interface is not public interface – it should be default interface
+
+String trafficCommisssionerr = "Navin "; //----12.Add variable String trafficCommisssioner = “Navin”;
+
+	
+//---------------------------------------------13.Add below methods
+
+	
+public void dontGoByDiselVehicle();  //--------14.void dontGoByDieselVehicle();
+
+	
+public void goByBicycle();//-------------------15. void goByBicycle();
+
+
+}
