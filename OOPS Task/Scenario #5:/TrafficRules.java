201dc004d917ac57f2a15f5ec23b868dd2133c8b
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-03 15:24:26 +0530

    Create TrafficRules.java

---
+++ OOPS Task/Scenario #5:/TrafficRules.java
@@ -0,0 +1,16 @@
+package tamilnadu.chennai;  //-------------------1.Create a package called tamilnadu.chennai
+
+public interface TrafficRules  //----------------2.Create an interface ‘TrafficRules’ under this package
+{                              //----------------3.Make sure this interface is public interface
+
+	
+String trafficCommisssioner = "Kavin"; //--------4.Add variable String trafficCommisssioner = “Kavin”;
+
+
+//-----------------------------------------------5.Add below methods
+	
+public void goByDiselVehicle();  //--------------5.1.void goByDieselVehicle();
+
+public void goBicycle();   //--------------------5.2.void goByBicycle();
+
+}
