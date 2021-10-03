6001f5f67e667f034168136896dddd059fa5dbe8
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-03 15:20:14 +0530

    Create SouthIndia.java

---
+++ OOPS Task/Scenario #4:/SouthIndia.java
@@ -0,0 +1,19 @@
+abstract class SouthIndia extends India  //---------------1.Create an abstract class called ‘SouthIndia’ ---2.Make this class as sub class of ‘India’
+{
+
+	
+//--------------------------------------------------------3.Add below non abstract methods
+void cultivate()  //--------------------------------------3.1.void cultivate()
+{
+System.out.println("Rice and Wheat cultivation");  //-----3.2.Print ‘Rice and Wheat cultivation’ inside this method
+}
+
+
+void livingStyle() //------------------------------------3.3.void livingStyle()
+{
+System.out.println("Average development");  //---------- 3.4.Print ‘Average development’ inside this method
+}
+
+
+
+}
