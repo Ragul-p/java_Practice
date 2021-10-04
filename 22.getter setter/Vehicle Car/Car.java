cc19e165a25ecad9893dfade82fcaf6be08c808d
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-04 19:51:26 +0530

    Create Car.java

---
+++ 22.getter setter/Vehicle Car/Car.java
@@ -0,0 +1,30 @@
+public class Car
+{
+
+public static void main (String[] args)
+{
+	
+	
+Vehicle gc = new Vehicle();
+	
+gc.setColor("red");
+System.out.println(gc.getColor());
+
+gc.setNum(100);
+System.out.println(gc.getNum());	
+	
+	
+}
+
+}
+
+
+/*
+
+output:-
+
+red
+100
+
+
+*/
