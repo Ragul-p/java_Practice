a8d42407d861fa4bb472e14d2676af65851d0c26
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-04 19:42:06 +0530

    Create Employee.java

---
+++ 20.Interface/Employee Kathir/Employee.java
@@ -0,0 +1,21 @@
+interface Employee
+{
+
+
+/*
+public Employee()
+{
+System.out.println("constructor ");	
+}
+
+ error: <identifier> expected
+
+*/
+
+int leaves = 10;
+
+public void doSomething();
+
+public void applyLeave();
+
+}
