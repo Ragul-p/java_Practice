372e7414cce5dadeaa445b295531c39b48def542
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-04 19:44:16 +0530

    Create AA.java

---
+++ 20.Interface/Rules AA BB/AA.java
@@ -0,0 +1,29 @@
+public class AA extends BB implements Rules
+{
+AA()
+{
+System.out.println("AA");
+}
+
+public static void main(String[] args)
+{
+
+AA a = new AA();
+
+}
+
+public void test()
+{
+}
+
+}
+
+
+/*
+
+output:-
+
+BB
+AA
+
+*/
