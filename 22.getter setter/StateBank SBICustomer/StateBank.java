ee77588aa0dcd000a875a6aac59765179c7f8376
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-04 19:54:41 +0530

    Create StateBank.java

---
+++ 22.getter setter/StateBank SBICustomer/StateBank.java
@@ -0,0 +1,19 @@
+public class StateBank
+{
+
+private int balance = 1000;
+
+//getter
+public int getBalance()
+{
+return this.balance;
+}
+
+//setter
+public void setBalance(int amount)
+{
+this.balance = this.balance + amount;
+}
+	
+	
+}
