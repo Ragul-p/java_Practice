bcc87c8652b735505bea89c088ff87088847eced
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-04 19:51:03 +0530

    Create Vehicle.java

---
+++ 22.getter setter/Vehicle Car/Vehicle.java
@@ -0,0 +1,31 @@
+public class Vehicle
+{
+
+private String color;
+//getter	
+public String getColor()
+{
+return color;
+}
+//setter
+public void setColor(String c)
+{
+this.color = c;	
+}
+
+	
+	
+private int num ;
+//getter	
+public int getNum()
+{
+return num;	
+}
+//setter	
+public void setNum(int n)
+{
+this.num = n;	
+}
+	
+
+}
