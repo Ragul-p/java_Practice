30d2fdfbf810200bdbf52d57ff315ef30c2a22d2
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-10-08 12:49:25 +0530

    Update Number_1_2_3_4_5.java

--- Conditional Statement/doWhile_while/Number_1_2_3_4_5.java
+++ Conditional Statement/doWhile_while/Number_1_2_3_4_5.java
@@ -3,23 +3,22 @@ package doWhile_While;
 public class Number_1_2_3_4_5
 {
 	
-
 	public static void main(String[] args)
 	  {
-      Number_1_2_3_4_5 n = new Number_1_2_3_4_5();
-      //n.num();
-        n.num2();
-    }
+           Number_1_2_3_4_5 n = new Number_1_2_3_4_5();
+          //n.num();
+            n.num2();
+          }
 
 	private void num2() 
 	    {
 		   
 		    int no = 1;
 		    do{
-			     System.out.println(no+ "  ");
-			     no=no+1;      // no=1, no=1+1=2, no=2+1=3, no=3+1=4, no=4+1=5
+	               System.out.println(no+ "  ");
+		       no=no+1;      // no=1, no=1+1=2, no=2+1=3, no=3+1=4, no=4+1=5
 		      }while(no<=5); //1<5 ,2<5   ,3<5   ,4<5   ,5<=5
-		}
+	    }
 
 	public void num() 
 	{    
