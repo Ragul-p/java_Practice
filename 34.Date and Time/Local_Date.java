029ca3e2a8b41813289e015a94fe9628169693db
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-11-01 15:11:20 +0530

    Create Local_Date.java

---
+++ 34.Date and Time/Local_Date.java
@@ -0,0 +1,23 @@
+package dateAndTime_1_11_2021;
+
+import java.time.LocalDate;
+
+public class Local_Date 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		LocalDate obj = LocalDate.now();
+		System.out.println(obj);
+			
+	}
+
+}
+/*
+
+output:-
+
+2021-11-01
+
+*/
