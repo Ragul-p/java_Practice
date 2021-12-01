b18fd1a5d35829d717c4abee9da4e84eab20f559
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-12-01 11:38:22 +0530

    Create Graduate.java

---
+++ 17.typecasting/Non-Primitive Types - Type Casting/Graduate.java
@@ -0,0 +1,26 @@
+package Object;
+
+public class Graduate extends Student
+{
+
+
+	public static void main(String[] args) 
+	{
+		Graduate s = new Graduate();
+		s.searchJob();	
+
+	}
+
+	public void searchJob()
+	{
+		
+		System.out.println("searchJob");
+	}
+	
+}
+/*
+
+output:-
+searchJob
+
+*/
